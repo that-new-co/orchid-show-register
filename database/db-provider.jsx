7e0af99c86f4e35c746804448976693c630d5ba6
chris-blanc <122929013+chris-blanc@users.noreply.github.com>
2024-01-21 09:30:33 -0500

    Fix DB name issue.  Remove cloud syncing.

--- database/db-provider.jsx
+++ database/db-provider.jsx
@@ -3,15 +3,9 @@ import { useEffect } from "react";
 import PouchDb from "pouchdb";
 import { Provider } from "use-pouchdb";
 
-const uri = process.env.IBM_CLOUDANT_API_URI;
-const test = process.env.GOOGLE_CLIENT_SECRET;
-console.log("[_app] test:", test);
-console.log("[_app] uri:", uri);
-
-const db_remote = new PouchDb(uri);
-// const db_remote = new PouchDb(
-// 	"https://apikey-v2-ccvcb2e0adna2wa7a7266w63uzyeid7r6js8nsv9dz1:d0effff001a849e572c0ae0c2485cc3b@b3c6740e-5250-4ac6-b9aa-f7561324f9cd-bluemix.cloudantnosqldb.appdomain.cloud/osr_remote"
-// );
+const db_remote = new PouchDb(
+	"https://apikey-v2-ccvcb2e0adna2wa7a7266w63uzyeid7r6js8nsv9dz1:d0effff001a849e572c0ae0c2485cc3b@b3c6740e-5250-4ac6-b9aa-f7561324f9cd-bluemix.cloudantnosqldb.appdomain.cloud/osr_remote"
+);
 // IBM Cloudant allows only certain characters in the database name.  lowercase letters, digits, and any of the characters _, $, (, ), +, -, and / are allowed. The database name must begin with a letter
 
 const DatabaseProvider = ({ db_name, children }) => {
